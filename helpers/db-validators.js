5ac9aeca010555d3abf52e0a092e28c77a9b41ed
ReaLdr <dilemajire@gmail.com>
2021-10-08 16:39:38 -0500

    CRUD Productos, categorías y búsqueda

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,30 +1,52 @@
 
- const Role = require('../models/role');
- const Usuario = require('../models/usuario');
- 
- const esRolValido = async( rol = '' ) =>{
-    const existeRol = await Role.findOne({ rol });
-    if(!existeRol){
-      throw new Error(`El rol ${rol} no está regustrado en la BD`);
-    }
-  }
+const { Categoria, Usuario, Producto } = require('../models');
+const Role = require('../models/role');
 
-  const emailExiste = async( correo = '') =>{
-    const existeEmail = await Usuario.findOne({ correo })
-    if( existeEmail ){
-        throw new Error(`El correo: ${correo} ya está registrado`);
-    }
+const esRolValido = async (rol = '') => {
+  const existeRol = await Role.findOne({ rol });
+  if (!existeRol) {
+    throw new Error(`El rol ${rol} no está regustrado en la BD`);
   }
+}
 
-  const existeUsuarioPorId = async( id ) =>{
-    const existeUsuario = await Usuario.findById( id )
-    if( !existeUsuario ){
-        throw new Error(`El id: ${id} no existe`);
-    }
+const emailExiste = async (correo = '') => {
+  const existeEmail = await Usuario.findOne({ correo })
+  if (existeEmail) {
+    throw new Error(`El correo: ${correo} ya está registrado`);
   }
+}
 
-  module.exports = {
-      esRolValido,
-      emailExiste,
-      existeUsuarioPorId
+const existeUsuarioPorId = async (id) => {
+  const existeUsuario = await Usuario.findById(id)
+  if (!existeUsuario) {
+    throw new Error(`El id: ${id} no existe`);
   }
+}
+
+/**
+ * 
+ * Categorias
+ */
+const existeCategoriaPorId = async (id) => {
+  const existeCategoria = await Categoria.findById(id)
+  if (!existeCategoria) {
+    throw new Error(`El id: ${id} no existe en las categorías`);
+  }
+}
+
+const existeProductoPorId = async (id) => {
+  const existeProducto = await Producto.findById(id)
+  if (!existeProducto) {
+    throw new Error(`El id: ${id} no existe en los productos`);
+  }
+}
+
+
+
+module.exports = {
+  esRolValido,
+  emailExiste,
+  existeUsuarioPorId,
+  existeCategoriaPorId,
+  existeProductoPorId
+}
--- models/server.js
+++ models/server.js
@@ -4,12 +4,20 @@ const { dbConnection } = require('../db/config');
 
 class Server {
 
-    constructor(){
+    constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        
-        this.usuariosPath = '/api/usuarios';
-        this.authPath = '/api/auth';
+
+        this.paths = {
+            auth:       '/api/auth',
+            buscar:     '/api/buscar',
+            categorias: '/api/categorias',
+            productos:  '/api/productos',
+            usuarios:   '/api/usuarios'
+        }
+
+        // this.usuariosPath = '/api/usuarios';
+        // this.authPath = '/api/auth';
 
         // Conectar a base de datos
         this.conectarDB();
@@ -21,33 +29,36 @@ class Server {
         this.routes();
     }
 
-    async conectarDB(){
+    async conectarDB() {
         await dbConnection();
     }
 
-    middlewares(){
+    middlewares() {
 
         // CORS
-        this.app.use( cors() );
+        this.app.use(cors());
 
         // Lectura y Parseo del body
-        this.app.use( express.json() );
+        this.app.use(express.json());
 
         //Directorio público
-        this.app.use( express.static('public') );
+        this.app.use(express.static('public'));
 
 
     }
 
     // Métodos
-    routes(){
+    routes() {
 
-        this.app.use( this.authPath, require('../routes/auth'));
-        this.app.use( this.usuariosPath, require('../routes/usuarios'));
+        this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
+        this.app.use(this.paths.categorias, require('../routes/categorias'));
+        this.app.use(this.paths.productos, require('../routes/productos'));
+        this.app.use(this.paths.usuarios, require('../routes/usuarios'));
     }
 
-    listen(){
-        this.app.listen(this.port, () =>{
+    listen() {
+        this.app.listen(this.port, () => {
             console.log('Server runing on port ', this.port);
         })
     }
